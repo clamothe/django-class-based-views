4f82b96205dc06eb51bf710203d8a5c8204558e8
Charlie <clamothe@clamothe.(none)>
2010-09-22 14:22:48 -0700

    Add necessary import and more doc.

--- class_based_views/jinja.py
+++ class_based_views/jinja.py
@@ -1,16 +1,21 @@
 from class_based_views import TemplateView
+from class_based_views.djangojinja2 import select_telpate
 
 class JinjaView(TemplateView):
     """
     Uses Jinja instead of django to render views.
-    Use as a mixin:
+    Use as a mixin, or as a replacement for TemplateView:
     
-    class MyView(JinjaView, DetailView):
+    class ViewA(JinjaView, DetailView): 
+        "DetailView rendered using Jinja"
+        ...
+    class ViewB(JinjaView):
+        "TemplateView rendered using Jinja"
         ...
     """                
     def load_template(self, names):
         """
-        Load a list of templates using the default template loader.
+        Load a list of templates using the Jinja template loader.
         """
         return select_template(names)
 
